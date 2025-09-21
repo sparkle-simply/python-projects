ad66be1afa01c88ebb5aa39982d6d67983f17179
Simran Sharma <sharmakammo13@gmail.com>
2025-09-21 21:23:51 +0530

    added default agrguments

--- Calculator/maths_calculator/maths_calculator.py
+++ Calculator/maths_calculator/maths_calculator.py
@@ -1,13 +1,13 @@
-def add(a, b):
+def add(a=0, b=0):
     return a + b
 
-def subtract(a, b):
+def subtract(a=0, b=0):
     return a - b
 
-def multiply(a, b):
+def multiply(a=1, b=1):
     return a * b
 
-def divide(a, b):
+def divide(a=1, b=1):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
