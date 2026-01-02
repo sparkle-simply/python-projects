c13a836a8a374fea370adfc60eff5d74316ce73d
Simran Sharma <sharmakammo13@gmail.com>
2026-01-02 21:20:30 +0530

    added ascii calc class impl

---
+++ Calculator/ascii_calculator/ascii_calculator_class_impl.py
@@ -0,0 +1,15 @@
+class ASCII_Calculator:
+
+    @staticmethod
+    def char_to_ascii(s = 'a'):
+        if not isinstance(s, str):
+            raise ValueError("Input must be string")
+        return [ord(char) for char in s]
+
+    @staticmethod
+    def ascii_to_char(ascii_values = [97]):
+        if isinstance(ascii_values, int):
+            ascii_values = [ascii_values]
+        if not all(0 <= val <= 127 for val in ascii_values):
+            raise ValueError("ASCII values should be within 0 and 127")
+        return ''.join(chr(val) for val in ascii_values)
