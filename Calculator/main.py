a0020a4126555028b4084952d74b8223356b1156
Simran Sharma <sharmakammo13@gmail.com>
2026-01-03 18:29:08 +0530

    added maths calc class impl

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,5 +1,5 @@
 import sys
-from maths_calculator import add, subtract, multiply, divide
+from maths_calculator import add, subtract, multiply, divide, Maths_Calculator
 from scientific_calculator import sine, cosine, tangent, logarithm, exponential
 from ascii_calculator import char_to_ascii, ascii_to_char, ASCII_Calculator
 from roman_calculator import int_to_roman
@@ -12,6 +12,20 @@ def maths_calculator():
     print("Multiplication: 3 * 4 =", multiply(3, 4))
     print("Division: 10 / 2 =", divide(10, 2))
     print("!! Thank You Maths Calculator !!")
+
+    print("\nHi, Maths Calculator Class Impl")
+    maths_util = Maths_Calculator()
+
+    try:
+        print("Addition: 6 + 7 =", maths_util.add(6, 7))
+        print("Subtraction: 14 - 6 =", maths_util.subtract(14, 6))
+        print("Multiplication: 212 * 13 =", maths_util.multiply(212, 13))
+        print("Division: 144 / 12 =", maths_util.divide(144, 12))
+
+    except ValueError as e:
+        print(f"Error: {e}")
+
+    print("!! Thank You Maths Calculator Class Impl !!")
 
 def scientific_calculator():
     print("\nHi, Scientific Calculator")
@@ -35,14 +49,14 @@ def ascii_calculator():
     print("!! Thank You ASCII Calculator !!")
 
     print("\nHi, ASCII Calculator Class Impl")
-    my_converter = ASCII_Calculator()
+    ascii_util = ASCII_Calculator()
 
     try:
         word = "Welcome"
-        ascii_vals = my_converter.char_to_ascii(word)
+        ascii_vals = ascii_util.char_to_ascii(word)
         print(f"'{word}' converted to ASCII: {ascii_vals}")
 
-        back_to_text = my_converter.ascii_to_char(ascii_vals)
+        back_to_text = ascii_util.ascii_to_char(ascii_vals)
         print(f"{ascii_vals} converted back: '{back_to_text}'")
 
     except ValueError as e:
--- Calculator/maths_calculator/__init__.py
+++ Calculator/maths_calculator/__init__.py
@@ -1,1 +1,2 @@
 from .maths_calculator import add, subtract, multiply, divide
+from .maths_calculator_class_impl import Maths_Calculator
