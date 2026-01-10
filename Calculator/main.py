92b9b69f25242ab012db48f417ba701bdfe47457
Simran Sharma <sharmakammo13@gmail.com>
2026-01-10 21:02:10 +0530

    added scientific calc uitility invocation

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,6 +1,6 @@
 import sys
 from maths_calculator import add, subtract, multiply, divide, Maths_Calculator
-from scientific_calculator import sine, cosine, tangent, logarithm, exponential
+from scientific_calculator import sine, cosine, tangent, logarithm, exponential, Scientific_Calculator
 from ascii_calculator import char_to_ascii, ascii_to_char, ASCII_Calculator
 from roman_calculator import int_to_roman
 
@@ -40,6 +40,26 @@ def scientific_calculator():
     print("exp 1 =", exponential(1))
     print("exp 0 =", exponential(0))
     print("!! Thank You Scientific Calculator !!")
+
+    print("\nHi, Scientific Calculator Class Impl")
+        scientific_util = Scientific_Calculator()
+
+        try:
+            print("sin(0) =", scientific_util.sine(0))
+            print("sin(90) = ", scientific_util.sine(90))
+            print("cos(0) =", scientific_util.cosine(0))
+            print("cos(90) = ", scientific_util.cosine(90))
+            print("tan(0) =", scientific_util.tangent(0))
+            print("tan(90) =", scientific_util.tangent(90))
+            print("log 100 base 10 =", scientific_util.logarithm(100, 10))
+            print("log 8 base 2 =", scientific_util.logarithm(8, 2))
+            print("exp 1 =", scientific_util.exponential(1))
+            print("exp 0 =", scientific_util.exponential(0))
+
+        except ValueError as e:
+            print(f"Error: {e}")
+
+        print("!! Thank You Scientific Calculator Class Impl !!")
 
 def ascii_calculator():
     print("\nHi, ASCII Calculator")
