abc59c6b0361ae8aa2520a614cc127d63b71b2c1
Simran Sharma <sharmakammo13@gmail.com>
2026-01-12 21:02:12 +0530

    matplotlib pyplot handson

--- Python-modules/Matplotlib/matplotlib-pyplot.py
+++ Python-modules/Matplotlib/matplotlib-pyplot.py
@@ -25,3 +25,27 @@ plt.show()
 # plotting multiple points with default xpoints
 plt.plot(multiple_y_points)
 plt.show()
+
+# plotting with markers
+plt.plot(multiple_y_points, marker = 'D')
+plt.show()
+
+# plotting markers with shortcut notation, marker|line|color
+plt.plot(multiple_y_points, 'o:r')
+plt.show()
+
+# plotting markers with marker size, ms
+plt.plot(multiple_y_points, marker = 'o', ms = 20)
+plt.show()
+
+# plotting markers with marker edge color, mec
+plt.plot(multiple_y_points, marker = 'o', ms = 20, mec = 'r')
+plt.show()
+
+# plotting markers with marker face color, mfc
+plt.plot(multiple_y_points, marker = 'o', ms = 20, mec = 'c')
+plt.show()
+
+# plotting markers with both mec and mfc
+plt.plot(multiple_y_points, marker = 'o', ms = 20, mec = '#4CAF50', mfc = '#4CAF50')
+plt.show()
