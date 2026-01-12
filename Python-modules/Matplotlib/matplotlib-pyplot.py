61aced50c46e1da4f488a74c0f698a4fb94697af
Simran Sharma <sharmakammo13@gmail.com>
2026-01-12 20:51:18 +0530

    matplotlib pyplot handson

--- Python-modules/Matplotlib/matplotlib-pyplot.py
+++ Python-modules/Matplotlib/matplotlib-pyplot.py
@@ -6,5 +6,22 @@ import numpy as np
 xpoints = np.array([0,7])
 ypoints = np.array([0,250])
 
+
+# plotting line between (0,7) and (0, 250), by default plot() function draws line from point to point
 plt.plot(xpoints, ypoints)
+plt.show()
+
+# plotting markers between (0,7) and (0, 250)
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+
+multiple_x_points = np.array([1,2,6,8])
+multiple_y_points = np.array([3,8,1,10])
+
+# plotting multiple lines
+plt.plot(multiple_x_points, multiple_y_points)
+plt.show()
+
+# plotting multiple points with default xpoints
+plt.plot(multiple_y_points)
 plt.show()
