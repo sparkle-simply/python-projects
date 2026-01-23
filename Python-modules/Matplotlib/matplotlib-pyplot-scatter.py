30b6458f56a72196072fa19a35aaeaa56373e2da
Simran Sharma <sharmakammo13@gmail.com>
2026-01-23 20:38:28 +0530

    matplotlib pyplot handson

--- Python-modules/Matplotlib/matplotlib-pyplot-scatter.py
+++ Python-modules/Matplotlib/matplotlib-pyplot-scatter.py
@@ -17,4 +17,15 @@ y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
 
 plt.scatter(x, y, c="green")
 
+# by default blue and orange color get plotted with scatter of 2 plots
+plt.show()
+
+
+# with scatter, we can plot each dots with different colors using colors array
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+
+plt.scatter(x, y, c=colors)
+
 plt.show()
