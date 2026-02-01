51d10dcd42a1bd7f71b0b822a4171a36ae9021a2
Simran Sharma <sharmakammo13@gmail.com>
2026-02-01 22:35:28 +0530

    ml basic concepts

--- Maching-Learning/ML-Basic-Concepts/ml-data-distribution.py
+++ Maching-Learning/ML-Basic-Concepts/ml-data-distribution.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# uniform data distribution uniform(low, high, values to be generated)
 x = np.random.uniform(0.0, 5.0, 100000)
 plt.hist(x, 5)
+plt.show()
+
+# normal data distribution normal(mean, standard deviation, values to be generated)
+x = np.random.uniform(5.0, 1.0, 100000)
+plt.hist(x, 100)
 plt.show()
