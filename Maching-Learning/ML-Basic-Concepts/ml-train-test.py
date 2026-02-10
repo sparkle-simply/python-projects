44fb8399c74ea53341df091f06d3c8d66d8b1294
Simran Sharma <sharmakammo13@gmail.com>
2026-02-10 19:31:17 +0530

    ml basic concepts

--- Maching-Learning/ML-Basic-Concepts/ml-train-test.py
+++ Maching-Learning/ML-Basic-Concepts/ml-train-test.py
@@ -25,6 +25,13 @@ plt.scatter(train_x, train_y)
 plt.plot(myline, mymodel(myline))
 plt.show()
 
-# getting r2 square value for traning set
+# getting r2 square value for training set
 r2 = r2_score(train_y, mymodel(train_x))
 print(r2)
+
+# getting r2 square value for testing set
+r2 = r2_score(test_y, mymodel(test_x))
+print(r2)
+
+# predicting new values using model when the relationship is 'ok' with fetched r2_square value
+print(mymodel(5))
