a622c35a2ca50d59bb6af296e4647e595a4a50f9
Simran Sharma <sharmakammo13@gmail.com>
2026-02-14 20:30:46 +0530

    ml hierarichal clustering concept

--- Maching-Learning/ML-Basic-Concepts/ml-hierarichal-clustering.py
+++ Maching-Learning/ML-Basic-Concepts/ml-hierarichal-clustering.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
+# from scipy.cluster.hierarchy import dendrogram, linkage
+
 
 x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
 y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
@@ -9,6 +11,10 @@ data = list(zip(x, y))
 
 hierarchical_cluster = AgglomerativeClustering(n_clusters=2, linkage='ward')
 labels = hierarchical_cluster.fit_predict(data)
+
+# linkage_data = linkage(data, method='ward', metric='euclidean')
+# dendrogram(linkage_data)
+
 
 plt.scatter(x, y, c=labels)
 plt.show()
