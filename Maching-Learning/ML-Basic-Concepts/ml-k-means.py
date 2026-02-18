b0a180998187608fbe6f8fa049e7431de643475c
Simran Sharma <sharmakammo13@gmail.com>
2026-02-18 21:32:51 +0530

    ml k means clustering

---
+++ Maching-Learning/ML-Basic-Concepts/ml-k-means.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+# K-means is an unsupervised learning method for clustering data points.
+# The algorithm iteratively divides data points into K clusters by minimizing the variance in each cluster.
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+plt.scatter(x, y)
+plt.show()
+
+data = list(zip(x, y))
+inertias = []
+
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(data)
+    inertias.append(kmeans.inertia_)
+
+plt.plot(range(1,11), inertias, marker='o')
+plt.title('Elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('Inertia')
+plt.show()
