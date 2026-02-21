6dc3174b0004eec3fa1276e89507fccde1323dd3
Simran Sharma <sharmakammo13@gmail.com>
2026-02-21 17:44:03 +0530

    ml cv leave one out approach

--- Maching-Learning/ML-Basic-Concepts/ml-decision-tree/ml-cross-validation.py
+++ Maching-Learning/ML-Basic-Concepts/ml-decision-tree/ml-cross-validation.py
@@ -1,6 +1,6 @@
 from sklearn import datasets
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
+from sklearn.model_selection import KFold, StratifiedKFold, LeaveOneOut, cross_val_score
 
 # When adjusting models we are aiming to increase overall model performance on unseen data. Hyperparameter tuning can lead to much better performance on test sets.
 # However, optimizing parameters to the test set can lead information leakage causing the model to preform worse on unseen data. To correct for this we can perform cross validation.
@@ -23,6 +23,16 @@ print("Number of CV Scores used in Average: ", len(scores))
 # To do so we can stratify the target classes, meaning that both sets will have an equal proportion of all classes.
 sk_folds = StratifiedKFold(n_splits = 5)
 scores = cross_val_score(clf, X, y, cv = sk_folds)
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+
+# Leave One Out (LOO)
+# Instead of selecting the number of splits in the training data set like k-fold LeaveOneOut,
+# utilize 1 observation to validate and n-1 observations to train.
+# This method is an exaustive technique.
+loo = LeaveOneOut()
+scores = cross_val_score(clf, X, y, cv = loo)
 print("Cross Validation Scores: ", scores)
 print("Average CV Score: ", scores.mean())
 print("Number of CV Scores used in Average: ", len(scores))
