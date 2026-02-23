20b0e5effef080dd38008c4494cfe17208a8e93c
Simran Sharma <sharmakammo13@gmail.com>
2026-02-23 21:44:10 +0530

    ml auc roc concept

--- Maching-Learning/ML-Basic-Concepts/ml-auc-roc-curve.py
+++ Maching-Learning/ML-Basic-Concepts/ml-auc-roc-curve.py
@@ -1,5 +1,16 @@
-import numpy as np
+import numpy as
+import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, roc_curve
+
+# AUC - ROC Curve
+# In classification, there are many different evaluation metrics.
+# The most popular is accuracy, which measures how often the model is correct.
+# This is a great metric because it is easy to understand and getting the most correct guesses is often desired.
+# There are some cases where you might consider using another evaluation metric.
+#
+# Another common metric is AUC, area under the receiver operating characteristic (ROC) curve.
+# The Reciever operating characteristic curve plots the true positive (TP) rate versus the false positive (FP) rate at different classification thresholds.
+# The thresholds are different probability cutoffs that separate the two classes in binary classification. It uses probability to tell us how well a model separates the classes.
 
 n = 10000
 ratio = .95
@@ -33,3 +44,21 @@ print('Confusion matrix')
 print(cf_mat)
 print(f'class 0 accuracy: {cf_mat[0][0]/n_0}')
 print(f'class 1 accuracy: {cf_mat[1][1]/n_1}')
+
+
+
+def plot_roc_curve(true_y, y_prob):
+    """
+    plots the roc curve based of the probabilities
+    """
+
+    fpr, tpr, thresholds = roc_curve(true_y, y_prob)
+    plt.plot(fpr, tpr)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+plot_roc_curve(y, y_proba)
+print(f'model 1 AUC score: {roc_auc_score(y, y_proba)}')
+
+plot_roc_curve(y, y_proba_2)
+print(f'model 2 AUC score: {roc_auc_score(y, y_proba_2)}')
