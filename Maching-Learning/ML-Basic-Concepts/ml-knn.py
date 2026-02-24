bd3342ba29d19bc8c1fea3e2a1d1d5bcef54f24e
Simran Sharma <sharmakammo13@gmail.com>
2026-02-24 18:51:13 +0530

    ml knn concept

--- Maching-Learning/ML-Basic-Concepts/ml-knn.py
+++ Maching-Learning/ML-Basic-Concepts/ml-knn.py
@@ -16,6 +16,7 @@ new_x = 8
 new_y = 21
 new_point = [(new_x, new_y)]
 
+# Using the input features and target class, we fit a KNN model on the model using 1 nearest neighbor
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(data, classes)
 prediction = knn.predict(new_point)
@@ -23,6 +24,7 @@ plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
 plt.text(x=new_x-1.7, y=new_y-0.7, s=f"new point, class: {prediction[0]}")
 plt.show()
 
+# Using the input features and target class, we fit a KNN model on the model using 5 nearest neighbor
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(data, classes)
 prediction = knn.predict(new_point)
