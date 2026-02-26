8620dd86f7e1af3d862436d8a7364fb229a4a942
Simran Sharma <sharmakammo13@gmail.com>
2026-02-26 16:01:12 +0530

    python dsa linked list

---
+++ Python-DSA/LinkedList/sll.py
@@ -0,0 +1,74 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+def traverseAndPrint(head):
+  currentNode = head
+  while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+  print("null")
+
+def insertNodeAtPosition(head, newNode, position):
+  if position == 1:
+    newNode.next = head
+    return newNode
+
+  currentNode = head
+  for _ in range(position - 2):
+    if currentNode is None:
+      break
+    currentNode = currentNode.next
+
+  newNode.next = currentNode.next
+  currentNode.next = newNode
+  return head
+
+def deleteSpecificNode(head, nodeToDelete):
+  if head == nodeToDelete:
+    return head.next
+
+  currentNode = head
+  while currentNode.next and currentNode.next != nodeToDelete:
+    currentNode = currentNode.next
+
+  if currentNode.next is None:
+    return head
+
+  currentNode.next = currentNode.next.next
+
+  return head
+
+def findLowestValue(head):
+  minValue = head.data
+  currentNode = head.next
+  while currentNode:
+    if currentNode.data < minValue:
+      minValue = currentNode.data
+    currentNode = currentNode.next
+  return minValue
+
+node1 = Node(7)
+node2 = Node(3)
+node3 = Node(2)
+node4 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+print("Original list:")
+traverseAndPrint(node1)
+
+# Insert a new node with value 97 at position 2
+newNode = Node(97)
+node1 = insertNodeAtPosition(node1, newNode, 2)
+
+print("\nAfter insertion:")
+traverseAndPrint(node1)
+
+# Delete node4
+node1 = deleteSpecificNode(node1, node4)
+
+print("The lowest value in the linked list is:", findLowestValue(node1))
