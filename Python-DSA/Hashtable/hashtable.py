bab59e58ed6657c89d215554606028e03c12e994
Simran Sharma <sharmakammo13@gmail.com>
2026-02-27 20:50:03 +0530

    python dsa hashtable

--- Python-DSA/Hashtable/hashtable.py
+++ Python-DSA/Hashtable/hashtable.py
@@ -1,4 +1,9 @@
+# Hash Table is a data structure designed to be fast to work with.
+#
+# The reason Hash Tables are sometimes preferred instead of arrays or linked lists is because searching for,
+# adding, and deleting data can be done really quickly, even for large amounts of data.
 
+# list with empty buckets and handling collisions for hashtable
 my_list = [
   [],
   [],
@@ -12,6 +17,7 @@ my_list = [
   []
 ]
 
+# hash function
 def hash_function(value):
   sum_of_chars = 0
   for char in value:
@@ -19,10 +25,12 @@ def hash_function(value):
 
   return sum_of_chars % 10
 
+# insert element in hashtable using hash function
 def add(name):
   index = hash_function(name)
   my_list[index].append(name)
 
+# implementation
 print("'Bob' has hash code:", hash_function('Bob'))
 
 add('Bob')
