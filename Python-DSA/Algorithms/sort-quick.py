f0c3ec8b96446e4bde4594538d2e979686f9fb34
Simran Sharma <sharmakammo13@gmail.com>
2026-03-05 20:37:52 +0530

    python dsa

--- Python-DSA/Algorithms/sort-quick.py
+++ Python-DSA/Algorithms/sort-quick.py
@@ -1,3 +1,7 @@
+#  Quicksort is one of the fastest sorting algorithms.
+#   The Quicksort algorithm takes an array of values, chooses one of the values as the 'pivot' element, and moves
+#   the other values so that lower values are on the left of the pivot element, and higher values are on the right of it.
+
 def partition(array, low, high):
   pivot = array[high]
   i = low - 1
