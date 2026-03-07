c752f69ef17cd6f04a4a69713f9c175fbecbf019
Simran Sharma <sharmakammo13@gmail.com>
2026-03-07 15:29:48 +0530

    python dsa

---
+++ Python-DSA/Algorithms/sort-merge.py
@@ -0,0 +1,36 @@
+# The Merge Sort algorithm is a divide-and-conquer algorithm that sorts an array by first breaking it down into smaller arrays,
+# and then building the array back together the correct way so that it is sorted.
+
+def mergeSort(arr):
+  if len(arr) <= 1:
+    return arr
+
+  mid = len(arr) // 2
+  leftHalf = arr[:mid]
+  rightHalf = arr[mid:]
+
+  sortedLeft = mergeSort(leftHalf)
+  sortedRight = mergeSort(rightHalf)
+
+  return merge(sortedLeft, sortedRight)
+
+def merge(left, right):
+  result = []
+  i = j = 0
+
+  while i < len(left) and j < len(right):
+    if left[i] < right[j]:
+      result.append(left[i])
+      i += 1
+    else:
+      result.append(right[j])
+      j += 1
+
+  result.extend(left[i:])
+  result.extend(right[j:])
+
+  return result
+
+mylist = [3, 7, 6, -10, 15, 23.5, 55, -13]
+mysortedlist = mergeSort(mylist)
+print("Sorted array:", mysortedlist)
