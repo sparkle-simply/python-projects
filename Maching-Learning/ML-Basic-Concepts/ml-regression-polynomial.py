3868780ea30a72abfcb8e6424f72381be8ca0978
Simran Sharma <sharmakammo13@gmail.com>
2026-03-08 17:54:29 +0530

    ml-regression-polynomial sanity correction

--- Maching-Learning/ML-Basic-Concepts/ml-regression-polynomial.py
+++ Maching-Learning/ML-Basic-Concepts/ml-regression-polynomial.py
@@ -8,19 +8,19 @@ x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
 y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
 
 # building custom polynomial model
-customModel = np.ploy1d(np.polyfit(x, y, 3))
+customModel = np.poly1d(np.polyfit(x, y, 3))
 # defining about line visibility from 1 to 22 position
-customLine = np.linespace(1, 22, 100)
+customLine = np.linspace(1, 22, 100)
 
 # original scatter plot
 plt.scatter(x, y)
 # polynomial regression
-plt.plot(customLine, customModel)
+plt.plot(customLine, customModel(customLine))
 # displaying diagram
 plt.show()
 
 # relationship measures as r-squared value that ranges from 0 to 1, where 0 means no relationship, and 1 means 100% related
-print(r2_score(y, customModel(x))
+print(r2_score(y, customModel(x)))
 
 # predicting speed of car that passes at 17:00 hours
 speed = customModel(17)
