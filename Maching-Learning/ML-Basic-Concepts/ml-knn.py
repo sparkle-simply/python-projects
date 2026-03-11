7914563b3d1edd1de51be3ae89f8f0f4d2593eec
Simran Sharma <sharmakammo13@gmail.com>
2026-03-11 23:59:26 +0530

    ml-k-means sanity correction

--- Maching-Learning/ML-Basic-Concepts/ml-knn.py
+++ Maching-Learning/ML-Basic-Concepts/ml-knn.py
@@ -25,7 +25,7 @@ plt.text(x=new_x-1.7, y=new_y-0.7, s=f"new point, class: {prediction[0]}")
 plt.show()
 
 # Using the input features and target class, we fit a KNN model on the model using 5 nearest neighbor
-knn = KNeighborsClassifier(n_neighbors=5)
+knn = KNeighborsClassifier(n_neighbors=6)
 knn.fit(data, classes)
 prediction = knn.predict(new_point)
 plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
