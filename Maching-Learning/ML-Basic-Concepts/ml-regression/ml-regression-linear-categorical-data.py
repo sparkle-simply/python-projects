7463a0b2b31a514d6e09e275ed81aa863e55bad2
Simran Sharma <sharmakammo13@gmail.com>
2026-03-15 13:53:44 +0530

    sanity check ml-regression impl

--- Maching-Learning/ML-Basic-Concepts/ml-regression/ml-regression-linear-categorical-data.py
+++ Maching-Learning/ML-Basic-Concepts/ml-regression/ml-regression-linear-categorical-data.py
@@ -11,7 +11,7 @@ y = cars['CO2']
 regr = linear_model.LinearRegression()
 regr.fit(X,y)
 
-##predict the CO2 emission of a VW where the weight is 2300kg, and the volume is 1300cm3:
+# predict the CO2 emission of a VW where the weight is 2300kg, and the volume is 1300cm3:
 predictedCO2 = regr.predict([[2300, 1300,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]])
 
 print(predictedCO2)
--- Maching-Learning/ML-Basic-Concepts/ml-regression/ml-regression-multiple.py
+++ Maching-Learning/ML-Basic-Concepts/ml-regression/ml-regression-multiple.py
@@ -4,7 +4,7 @@ from sklearn import linear_model
 # multiple regression is linear regression where more than one independent values are used to predict the (dependent) value
 
 # pandas module helps in reading csv files and creating data frame objects
-df = pandas.read("data.csv")
+df = pandas.read_csv("data.csv")
 
 X = df[['Weight', 'Volume']]
 y = df['CO2']
@@ -14,7 +14,7 @@ regr = linear_model.LinearRegression()
 regr.fit(X, y)
 
 # predict CO2 emission for car with weight: 2300 and volume: 1300
-predictedCO21 = regr.predict([[2300, 1300])
+predictedCO21 = regr.predict([[2300, 1300]])
 print(predictedCO21)
 
 # coefficient is factor that describes the relationship with unknown variable
@@ -22,7 +22,7 @@ print(regr.coef_)
 
 # if we increase weight by 1000 kg, the outcome will change based on coefficient
 # here, predictedCO22 = predictedCO21 + (1000 * regr.coefficient(weight))
-predictedCO22 = regr.predict([[3300, 1300])
+predictedCO22 = regr.predict([[3300, 1300]])
 print(predictedCO22)
 
 
